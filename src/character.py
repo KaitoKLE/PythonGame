92fd3af5fe26cacf30fa24d57025772de9470293
KaitoKLE <luiserkrazy@gmail.com>
2023-12-20 20:27:29 -0500

    Revert "Implementing tile-based movement!"
    This reverts commit 4b1bb209a20405d2ddf010b898dd41a0f6a9d7c0.

--- src/character.py
+++ src/character.py
@@ -1,47 +1,27 @@
-from constants import TILES_SIZE
-
-
 class Character:
     def __init__(self, pos, name, sprite):
         self.name = name
         self.__pos = list(pos)
-        self.__speed = [0, 0]
-        self._x = 0
-        self._y = 0
+        self.dx = 0
+        self.dy = 0
         self.sprite_data = sprite
-    
-    @property
-    def col(self):
-        return self.__pos[0]
-    
-    @col.setter
-    def col(self, new_x):
-        self.__pos[0] = new_x
-    
-    @property
-    def row(self):
-        return self.__pos[1]
-    
-    @row.setter
-    def row(self, new_y):
-        self.__pos[1] = new_y
 
     @property
     def x(self):
-        return self._x
+        return self.__pos[0]
+
+    @x.setter
+    def x(self, new_x):
+        self.__pos[0] = new_x
 
     @property
     def y(self):
-        return self._y
-    
-    @property
-    def speed(self):
-        return self.__speed
-    
-    @speed.setter
-    def speed(self, new_value):
-        self.__speed = list(new_value)
+        return self.__pos[1]
 
-    # def move(self):
-    #     self.__pos[0] += self.dx
-    #     self.__pos[1] += self.dy
+    @y.setter
+    def y(self, new_y):
+        self.__pos[1] = new_y
+
+    def move(self):
+        self.__pos[0] += self.dx
+        self.__pos[1] += self.dy
--- src/display.py
+++ src/display.py
@@ -6,22 +6,22 @@ import pygame.gfxdraw
 from constants import MAIN_PATH, W_MIN_WIDTH, W_MIN_HEIGHT, MOVEMENT_DEFAULT_SPEED
 
 
-# def detect_collisions(ch1, ch2):
-#     prev_movement_dir = (ch1.dx, ch1.dy)
-#     c1_rect = pygame.Rect(ch1.x, ch1.y, ch1.sprite_data.size, ch1.sprite_data.size)
-#     c2_rect = pygame.Rect(ch2.x, ch2.y, ch2.sprite_data.size, ch2.sprite_data.size)
-#     if c1_rect.colliderect(c2_rect):
-#         ch1.dx = 0
-#         ch1.dy = 0
-#         if prev_movement_dir[0] < 0:  # left
-#             c1_rect.move_ip(MOVEMENT_DEFAULT_SPEED, 0)
-#         elif prev_movement_dir[0] > 0:  # right
-#             c1_rect.move_ip(-MOVEMENT_DEFAULT_SPEED, 0)
-#         if prev_movement_dir[1] < 0:  # up
-#             c1_rect.move_ip(0, MOVEMENT_DEFAULT_SPEED)
-#         elif prev_movement_dir[1] > 0:  # down
-#             c1_rect.move_ip(0, -MOVEMENT_DEFAULT_SPEED)
-#         return c1_rect.x, c1_rect.y
+def detect_collisions(ch1, ch2):
+    prev_movement_dir = (ch1.dx, ch1.dy)
+    c1_rect = pygame.Rect(ch1.x, ch1.y, ch1.sprite_data.size, ch1.sprite_data.size)
+    c2_rect = pygame.Rect(ch2.x, ch2.y, ch2.sprite_data.size, ch2.sprite_data.size)
+    if c1_rect.colliderect(c2_rect):
+        ch1.dx = 0
+        ch1.dy = 0
+        if prev_movement_dir[0] < 0:  # left
+            c1_rect.move_ip(MOVEMENT_DEFAULT_SPEED, 0)
+        elif prev_movement_dir[0] > 0:  # right
+            c1_rect.move_ip(-MOVEMENT_DEFAULT_SPEED, 0)
+        if prev_movement_dir[1] < 0:  # up
+            c1_rect.move_ip(0, MOVEMENT_DEFAULT_SPEED)
+        elif prev_movement_dir[1] > 0:  # down
+            c1_rect.move_ip(0, -MOVEMENT_DEFAULT_SPEED)
+        return c1_rect.x, c1_rect.y
 
 
 class Display:
@@ -37,25 +37,25 @@ class Display:
             logging.error(f'Cannot load the image file: {e}')
         pygame.display.set_caption('Python game')
 
-    # @property
-    # def center(self):
-    #     return int(self.width / 2), int(self.height / 2)
+    @property
+    def center(self):
+        return int(self.width / 2), int(self.height / 2)
 
     def draw(self, c_map, player):
         c_map.tile_map.render(self.canvas)
-        # for npc in c_map.npc:
-        #     self.draw_character(npc)
+        for npc in c_map.npc:
+            self.draw_character(npc)
         self.draw_character(player)
         pygame.display.flip()
 
     def background(self, color):
         pygame.draw.rect(self.canvas, color, (0, 0, self.width, self.height))
 
-    # def update_size(self, new_w, new_h):
-    #     logging.info('Updating display size...')
-    #     self.canvas = pygame.display.set_mode((new_w, new_h), False)
-    #     self.width = new_w
-    #     self.height = new_h
+    def update_size(self, new_w, new_h):
+        logging.info('Updating display size...')
+        self.canvas = pygame.display.set_mode((new_w, new_h), False)
+        self.width = new_w
+        self.height = new_h
 
     def draw_character(self, character):
         character.sprite_data.sprite = pygame.draw.rect(
